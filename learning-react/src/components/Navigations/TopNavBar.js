1fe152dfabc8b2acbfc2a95405282b48b0464775
Maksym <davydovmaksym9@gmail.com>
2022-07-21 12:28:34 +0200

    Creating sign in system, registration. Displaying, saving user name on firebase

--- learning-react/src/components/Navigations/TopNavBar.js
+++ learning-react/src/components/Navigations/TopNavBar.js
@@ -1,8 +1,11 @@
-import { isCursorAtEnd } from "@testing-library/user-event/dist/utils";
 import React from "react";
 import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import my_photo_1 from "../../my_photo_1.png"
+import my_photo_1 from "../../my_photo_1.png";
+import { auth, db } from "../../firebase";
+import { signOut } from "firebase/auth";
+import { onValue, ref } from 'firebase/database';
+
 
 class User {
     constructor(){
@@ -11,18 +14,47 @@ class User {
     }
 }
 
+const handleSignOut = () => {
+    signOut(auth).catch(err => console.log(err))
+}
+
 function TopNavBar() {
     const curr = new User();
+    const [UserName, setUserName] = React.useState("");
+
+    React.useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+          if (user) {
+            onValue(ref(db, `/users`), (snapshot) => {
+                setUserName("");
+                const data = snapshot.val();
+                Object.values(data).find((_user) => {
+                    if(user.uid !== _user.uuid) {
+
+                    }
+                    else if (user.uid === _user.uuid) {
+                        setUserName(_user.name)
+                        
+                    }
+                });
+            });
+          } 
+          else if (!user) {
+
+          }
+        });
+    }, []);
+
     return (
         <div className="w-full h-12 border-b-2">
             <div className="relative w-max--75 h-full left-12 flex justify-between items-center ">
                     <div className="min-w-fit">
-                        Hello, {curr.UserName}!
+                        Hello, {UserName}!
                     </div>
                     <div className="w-56 max-w-lg min-w-fit h-full flex items-center justify-between">
                         <div className="mr-6">Left: {curr.LeftDays} day(s)</div>
                         <div className="mr-6"><img src={my_photo_1} className="w-8 h-8 rounded-full"></img></div>
-                        <FontAwesomeIcon icon={faArrowRightFromBracket} className="mr-6"/>
+                        <FontAwesomeIcon onClick={handleSignOut} icon={faArrowRightFromBracket} className="mr-6"/>
                     </div>
             </div>
         </div>
--- learning-react/src/components/SignIn.js
+++ learning-react/src/components/SignIn.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../firebase.js';
-import { useNavigate } from "react-router-dom";
-import TransitionComponent from './TransitionComponent.js';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { auth, db } from '../firebase.js';
+import { ref, set } from 'firebase/database';
 
 export default function SignIn() {
+    const NameRef = React.useRef();
     const EmailRef = React.useRef();
     const PasswordRef = React.useRef();
     const [show, setShow] = React.useState(true);
+    const [showRegister, setShowRegister] = React.useState(false);
 
-    React.useEffect(()=> {
+    React.useEffect(() => {
         auth.onAuthStateChanged(user => {
             if(user) {
                 setShow(false)
@@ -20,10 +21,31 @@ export default function SignIn() {
         })
     }, [])
 
+    React.useEffect(() => {
+        setShow(prev => !prev)
+    }, [auth])
+
     const handleSignIn = (e) => {
         e.preventDefault();
         signInWithEmailAndPassword(auth, EmailRef.current.value, PasswordRef.current.value)
-        .then(()=>setShow(false))
+        .catch(err => console.log(err))
+    }
+
+    const writeName = () => {
+        auth.onAuthStateChanged(user => {
+            const uuid = user.uid;
+            set(ref(db, `/users/${uuid}`), {
+                name: NameRef.current.value,
+                uuid: uuid,
+            })
+        })
+        
+    } 
+
+    const handleCreate = (e) => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, EmailRef.current.value, PasswordRef.current.value)
+        .then(writeName)
         .catch(err => console.log(err))
     }
 
@@ -32,24 +54,51 @@ export default function SignIn() {
             { show ?
             <div className='absolute left-0 top-0 w-full h-full bg-blue-200 z-10 flex justify-center items-center'>
                 <div className="z-20 w-96 h-120 bg-white flex flex-col justify-between rounded-xl shadow-xl">
-                    <form onSubmit={handleSignIn}>
-                        <div className="w-full h-20 flex items-center justify-start ml-2 text-2xl font-bold">
-                            Sign in
-                        </div>
-                        <div className='relative w-5/6 h-fit ml-10'>
-                            <div className="w-full h-20 flex justify-start items-center">
-                                <label className='font-bold'>Email</label>
-                                <input type="email" ref={EmailRef} placeholder="example.example.com" className='absolute right-0 border-2'></input>                        
+                    { !showRegister ?
+                        <form onSubmit={handleSignIn}>
+                            <div className="w-full h-20 flex items-center justify-start ml-2 text-2xl font-bold">
+                                Sign in
                             </div>
-                            <div className="w-full h-20 flex justify-start items-center">
-                                <label className='font-bold'>Password</label>
-                                <input type="password" ref={PasswordRef} placeholder="Password" className='absolute right-0 border-2'></input>
+                            <div className='relative w-5/6 h-fit ml-10'>
+                                <div className="w-full h-20 flex justify-start items-center">
+                                    <label className='font-bold'>Email</label>
+                                    <input type="email" ref={EmailRef} placeholder="example.example.com" className='absolute right-0 border-2'></input>                        
+                                </div>
+                                <div className="w-full h-20 flex justify-start items-center">
+                                    <label className='font-bold'>Password</label>
+                                    <input type="password" ref={PasswordRef} placeholder="Password" className='absolute right-0 border-2'></input>
+                                </div>
                             </div>
-                        </div>
-                        <div className="w-full h-20 flex justify-center items-center">
-                            <button type='submit' className="w-24 h-10 bg-green-apple rounded-xl">Sign in</button>
-                        </div>
-                    </form>
+                            <div className="w-full h-40 flex flex-col justify-center items-center">
+                                <button type='submit' className="w-24 h-10 bg-green-apple rounded-xl">Sign in</button>
+                                <button onClick={() => (setShowRegister(true), EmailRef.current.value ="", PasswordRef.current.value ="")} className="w-44 h-6 bg-red-400 rounded-xl mt-2 text-sm">Create an account</button>
+                            </div>
+                        </form>
+                        :
+                        <form onSubmit={handleCreate}>
+                            <div className="w-full h-20 flex items-center justify-start ml-2 text-2xl font-bold">
+                                Create an account
+                            </div>
+                            <div className='relative w-5/6 h-fit ml-10'>
+                                <div className="w-full h-20 flex justify-start items-center">
+                                    <label className='font-bold'>Name</label>
+                                    <input type="text" ref={NameRef} placeholder="Daniel Grey" className='absolute right-0 border-2'></input>                        
+                                </div>
+                                <div className="w-full h-20 flex justify-start items-center">
+                                    <label className='font-bold'>Email</label>
+                                    <input type="email" ref={EmailRef} placeholder="example.example.com" className='absolute right-0 border-2'></input>                        
+                                </div>
+                                <div className="w-full h-20 flex justify-start items-center">
+                                    <label className='font-bold'>Password</label>
+                                    <input type="password" ref={PasswordRef} placeholder="Password" className='absolute right-0 border-2'></input>
+                                </div>
+                            </div>
+                            <div className="w-full h-40 flex flex-col justify-center items-center">
+                                <button type='submit' className="w-24 h-10 bg-green-apple rounded-xl">Create</button>
+                                <button onClick={() => (setShowRegister(false), EmailRef.current.value ="", PasswordRef.current.value ="")} className="w-44 h-6 bg-red-400 rounded-xl mt-2 text-sm">Back to sign in</button>
+                            </div>
+                        </form>
+                    }
                 </div>
             </div>
             : <div></div>
