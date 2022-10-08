906612b0468eae2609e5060cde6d4cd5b7e62ea2
Maksym <davydovmaksym9@gmail.com>
2022-10-08 16:56:42 +0200

    Added loading screen

--- learning-react/src/components/Navigations/UserSettings.js
+++ learning-react/src/components/Navigations/UserSettings.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CalendarContext } from '../../Home';
-import PersonPNG from "../../PersonPNG.png";
+import PersonPNG from "../../images/PersonPNG.png";
 import { faPencil, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import EditPhotoPopUp from './EditPhotoPopUp';
--- learning-react/src/firebase.js
+++ learning-react/src/firebase.js
@@ -6,15 +6,24 @@ import { getStorage } from "firebase/storage";
 import { getFunctions } from "firebase/functions"
 
 const firebaseConfig = {
-  apiKey: "apiKey",
-  authDomain: "authDomain",
-  projectId: "projectId",
-  storageBucket: "storageBucket",
-  messagingSenderId: "messagingSenderId",
-  appId: "appId",
-  measurementId: "measurementId",
-  appAssociation: "appAssociation",
-  rewrites: [ { "source": "/room/**", "dynamicLinks": true } ]
+  apiKey: "AIzaSyAEmHs98rkncHla_UrNOzOeBIyiofb6ZFQ",
+  authDomain: "vacateme-75946.firebaseapp.com",
+  projectId: "vacateme-75946",
+  storageBucket: "vacateme-75946.appspot.com",
+  messagingSenderId: "773638070568",
+  appId: "1:773638070568:web:ae06bfa59cc561400bf01d",
+  measurementId: "G-VPFT9GFHEG",
+  appAssociation: "AUTO",
+  rewrites: [ { "source": "/room/**", "dynamicLinks": true } ],
+
+// Another project
+  // apiKey: "AIzaSyCnoXs6ghN4Rb3_ZxWh9RccAa37FRyV9-c",
+  // authDomain: "civil-planet-357119.firebaseapp.com",
+  // projectId: "civil-planet-357119",
+  // storageBucket: "civil-planet-357119.appspot.com",
+  // messagingSenderId: "943210427754",
+  // appId: "1:943210427754:web:1dca465fcffc3b8cd56113",
+  // measurementId: "G-PCS8NHRK99"
 };
 
 // Initialize Firebase
